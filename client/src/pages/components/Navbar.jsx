5726c92bae28a8f45ae1343ea7d30edee8eff4c7
pocoyo <tomibanderas20@gmail.com>
2024-05-08 18:57:37 -0300

    h2 mensajito

--- client/src/pages/components/Navbar.jsx
+++ client/src/pages/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <>
         <header>
-            <h1>Croto Blog</h1><br /><small>el espacio para hablar y reirse con familia y amigos</small>
+            <h1>Croto Blog</h1><br /><h2><small>el espacio para hablar y reirse con familia y amigos</small></h2>
             <ul>
                 <li><h2><Link to={'/'}>Casita</Link></h2></li>
                 <li><h2><Link to={'/new'}>Postea algo</Link></h2></li>
