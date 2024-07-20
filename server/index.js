49452940c58c870d81cbfe61a62a97af0e35cf88
pocoyo <tomibanderas20@gmail.com>
2024-07-20 20:54:08 -0300

    cors posible solucion

--- server/index.js
+++ server/index.js
@@ -1,19 +1,31 @@
 import express from 'express'
 import cors from 'cors'
 import {PORT} from './config.js'
-import indexRoutes from './routes/index.routes.js'
+// import indexRoutes from './routes/index.routes.js'
 import blogRoutes from './routes/blogs.routes.js'
 
 const app = express()
 
 app.use(express.json())
-const corsOption = {
-    credentials: true,
-    origin: ['http://localhost:5173', 'crotoblog.netlify.app']
-}
-app.use(cors(corsOption))
+// app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    next();
+  });
+app.options('/*', (_, res) => {
+    res.sendStatus(200);
+});
 
-app.use(indexRoutes)
+
+// app.use(indexRoutes)
 app.use(blogRoutes)
 
 app.listen(PORT)
