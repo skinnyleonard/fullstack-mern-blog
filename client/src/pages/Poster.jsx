13980ca0521de5e6fdaa1791f14d125434cf5ed3
pocoyo <tomibanderas20@gmail.com>
2024-07-20 21:07:36 -0300

    status

--- client/src/pages/Poster.jsx
+++ client/src/pages/Poster.jsx
@@ -13,6 +13,7 @@ function Poster() {
     console.log(targeted)
     try {
       const response = await createBlogRequest(targeted)
+      response.status = 200;
       console.log(response)
     } catch (error) {
       console.error(error)
