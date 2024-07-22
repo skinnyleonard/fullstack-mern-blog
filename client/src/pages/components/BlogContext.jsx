1e3ac8061fb44087f729a587145ae7d5f16b62b0
pocoyo <tomibanderas20@gmail.com>
2024-07-22 19:43:42 -0300

    sistema de imagenes 2

--- client/src/pages/components/BlogContext.jsx
+++ client/src/pages/components/BlogContext.jsx
@@ -23,7 +23,7 @@ export const BlogContextProvider = ({ children }) => {
 
   function loadBlogs(){
     setIsloading(true)
-    fetch('http://localhost:4000/blogs')
+    fetch('https://fullstack-mern-blog.onrender.com/blogs')
     .then(response => response.json())
     .then(json => {setBlogs(json); setIsloading(false)})
   }
@@ -39,7 +39,7 @@ export const BlogContextProvider = ({ children }) => {
   }
   async function loadComments (id){
     setIsloading(true)
-    await fetch(`http://localhost:4000/comments/${id}`)
+    await fetch(`https://fullstack-mern-blog.onrender.com/comments/${id}`)
     .then(res => res.json())
     .then(json => {setComments(json); setIsloading(false)})
   }
